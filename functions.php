028c36229d93c768ca37a5687fb940dff81a8b06
Alfredo <aacuello@gmail.com>
2018-04-23 19:46:13 -0400

    second commit

--- functions.php
+++ functions.php
@@ -10,13 +10,24 @@ function enqueue_parent_style(){
 function mychildtheme_enqueue_styles() {
 $parent_style = 'twentyseventeen';
 
-wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+wp_enqueue_style( $parent_style, get_template_directory_uri() . '/drill/style.css' );
 wp_enqueue_style( 'child-style',
-get_stylesheet_directory_uri() . '/style.css',
+get_stylesheet_directory_uri() . '/drill/style.css',
 array( $parent_style )
 );
 }
 add_action( 'wp_enqueue_scripts', 'mychildtheme_enqueue_styles' );
-wp_enqueue_script('main', get_stylesheet_directory_uri() . '/main.js',false,'1.1',"all");
+//wp_enqueue_script('main', get_stylesheet_directory_uri() . '/main.js',false,'1.1',"all");
+
+function adding_scripts() {
+
+    wp_register_script('main',get_stylesheet_directory_uri() . '/drill/main.js', array('jquery'),'1.1', true);
+    wp_enqueue_script('main');
+    }
+    
+    add_action( 'wp_enqueue_scripts', 'adding_scripts' );
+
+
+
 
 ?>
---
+++ page-drill.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Drill 1</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="/Drill/wp-content/themes/DrillTheme/style.css" />
+   
+</head>
+<body>
+    
+    
+    <h1>Drill 1</h1>
+    
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non voluptatem eius commodi itaque consequuntur minus, reiciendis aliquam placeat, est at nihil fugiat animi eos vel voluptate, nam quam praesentium expedita blanditiis tempora vero nobis dicta veritatis nostrum! Optio at corporis incidunt ullam fuga adipisci beatae reprehenderit blanditiis? Aspernatur sit corrupti sequi dignissimos vero alias ipsum consequatur provident! Iure, fugiat at.</p>
+
+    
+
+     <script src="/Drill/wp-content/themes/DrillTheme/main.js"></script>
+</body>
+</html>
